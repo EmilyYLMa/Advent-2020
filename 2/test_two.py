76f1b58b961a8ef500edb5bb27e2b2346cf7c7df
Emily Ma <ema@opusonesolutions.com>
2021-03-21 22:43:53 -0400

    Add more test cases to Day 2

--- 2/test_two.py
+++ 2/test_two.py
@@ -7,9 +7,10 @@ from two import (
 
 
 @pytest.mark.parametrize('entry,expected_result', [
-   ('2-3 x: xxrk', True),  # test correct if equal to min for lowercase
-   ('2-3 X: XXrK', True),  # test correct if equal to min for uppercase
-   ('2-3 x: xxrxk', True),  # test correct if equal to max
+   ('2-3 x: xxrk', True),  # test lower-case target count equal to min
+   ('2-3 X: XXrK', True),  # test upper-case target count equal to min
+   ('2-3 x: xxrxk', True),  # test target count equal to max
+   ('2-4 x: xxxrxk', True),  # test target count in between min or max
    ('2-3 x: xrk', False),  # test does not respect min
    ('2-3 x: xxxxxrk', False),  # test does not respect max
 ])
@@ -19,10 +20,11 @@ def test_check_password_1(entry, expected_result):
 
 
 @pytest.mark.parametrize('entry,expected_result', [
-   ('1-3 a: abcde', True),
-   ('1-3 A: ABCDE', True),
-   ('1-3 b: cdefg', False),
-   ('2-9 c: cccccccccc', False),
+   ('1-3 a: abcde', True),  # test lower-case target on index 1
+   ('1-3 A: ABCDE', True),  # test upper-case target on index 1
+   ('1-3 A: BBADE', True),  # test upper-case target on index 2
+   ('1-3 b: cdefg', False),  # test target not on index 1 or index 2
+   ('2-9 c: cccccccccc', False),  # test target on index 1 and index 2
 ])
 def test_check_password_2(entry, expected_result):
     res = is_valid_password_2(entry)
