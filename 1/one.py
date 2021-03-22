cba5e8327d124b2aec7f505111dfda611ce493e1
Emily Ma <ema@opusonesolutions.com>
2021-03-21 22:10:11 -0400

    Add solutions and tests for Day 1

---
+++ 1/one.py
@@ -0,0 +1,54 @@
+import argparse
+
+
+class NoEntriesFound(Exception):
+    pass
+
+
+def find_two_entries(data, target_number=2020):
+    for x in data:
+        y = target_number - x
+        if y in data:
+            return x, y, x * y
+    else:
+        raise NoEntriesFound(
+            f'There are no two entries that add up to {target_number}'
+        )
+
+
+def find_three_entries(data, target_number=2020):
+    for x in data:
+        for y in data:
+            z = target_number - x - y
+            if z in data:
+                return x, y, z, x * y * z
+    else:
+        raise NoEntriesFound(
+            f'There are no three entries that add up to {target_number}'
+        )
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f', '--filename',
+        required=True,
+        help='Filename of input data.'
+    )
+    args = parser.parse_args()
+
+    with open(args.filename) as f:
+        data = [float(x) for x in f]
+
+    try:
+        x, y, z = find_two_entries(data, 2020)
+        print(f'Product of the Two Entries that Add Up to 2020: {z}')
+    except NoEntriesFound as e:
+        print(e)
+
+    try:
+        x, y, z, w = find_three_entries(data, 2020)
+        print(f'Product of the Three Entries that Add Up to 2020: {w}')
+    except NoEntriesFound as e:
+        print(e)
---
+++ 1/test_one.py
@@ -0,0 +1,48 @@
+import pytest
+from one import (
+    find_two_entries,
+    find_three_entries,
+    NoEntriesFound
+)
+
+
+@pytest.mark.parametrize("data,target_number", [
+    ([1, 2, 4, 5, 8], 10),
+    ([1, -2, 4, 5, 8], -1),  # test negative target number
+    ([1.5, 2, 4, 5.5, 8], 7),  # test entries can be floats
+])
+def test_find_two_entries(data, target_number):
+    x, y, z = find_two_entries(data, target_number)
+
+    assert x + y == target_number
+    assert z == x*y
+
+
+@pytest.mark.parametrize('data,target_number', [
+    ([1, 2, 4, 5, 8], 100),
+    ([], 100),
+])
+def test_no_two_entries_add_to_target(data, target_number):
+    with pytest.raises(NoEntriesFound):
+        find_two_entries(data, target_number)
+
+
+@pytest.mark.parametrize("data,target_number", [
+    ([1, 2, 3, 4, 5], 6),
+    ([1, 2, -3, 4, 5], -2),
+    ([1.5, 2, 4, 5.5, 8], 9),  # test entries can be floats
+])
+def test_find_three_entries(data, target_number):
+    x, y, z, w = find_three_entries(data, target_number)
+
+    assert x + y + z == target_number
+    assert w == x * y * z
+
+
+@pytest.mark.parametrize('data,target_number', [
+    ([1, 2, 3, 4, 5], 100),
+    ([], 100),
+])
+def test_no_three_entries_add_to_target(data, target_number):
+    with pytest.raises(NoEntriesFound):
+        find_three_entries(data, target_number)
